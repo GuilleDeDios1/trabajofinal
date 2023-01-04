e07ad87e357d68805ba6e40cd2a08ab095723047
Guillermo De Dios <guille_dedios03@hotmail.com>
2023-01-04 03:10:53 -0300

    trabajo terminado

---
+++ cv/funcionalidad/function.js
@@ -0,0 +1,63 @@
+//calcula los años para los datos personales
+var fechaactual = new Date();
+var fechanacimiento = new Date(1963, 1, 5)
+document.getElementById("cumpleaños").innerHTML += fechaactual.getFullYear()-fechanacimiento.getFullYear();
+
+//botones ver mas
+document.getElementById("btnvermas1").addEventListener("click",function(){
+    document.getElementById("vermas1").style = 'display:block';
+    document.getElementById("btnvermas1").style = 'display:none';
+    document.getElementById("btnvermenos1").style = 'display:block';
+})
+
+document.getElementById("btnvermas2").addEventListener("click",function(){
+    document.getElementById("vermas2").style = 'display:block';
+    document.getElementById("btnvermas2").style = 'display:none';
+    document.getElementById("btnvermenos2").style = 'display:block';
+})
+
+document.getElementById("btnvermas3").addEventListener("click",function(){
+    document.getElementById("vermas3").style = 'display:block';
+    document.getElementById("btnvermas3").style = 'display:none';
+    document.getElementById("btnvermenos3").style = 'display:block';
+})
+
+document.getElementById("btnvermas4").addEventListener("click",function(){
+    document.getElementById("vermas4").style = 'display:block';
+    document.getElementById("btnvermas4").style = 'display:none';
+    document.getElementById("btnvermenos4").style = 'display:block';
+})
+
+document.getElementById("btnvermas5").addEventListener("click",function(){
+    document.getElementById("vermas5").style = 'display:block';
+    document.getElementById("btnvermas5").style = 'display:none';
+    document.getElementById("btnvermenos5").style = 'display:block';
+})
+
+
+//botones ver menos
+document.getElementById("btnvermenos1").addEventListener("click",function(){
+    document.getElementById("vermas1").style = 'display:none';
+    document.getElementById("btnvermenos1").style = 'display:none';
+    document.getElementById("btnvermas1").style = 'display:block';
+})
+document.getElementById("btnvermenos2").addEventListener("click",function(){
+    document.getElementById("vermas2").style = 'display:none';
+    document.getElementById("btnvermenos2").style = 'display:none';
+    document.getElementById("btnvermas2").style = 'display:block';
+})
+document.getElementById("btnvermenos3").addEventListener("click",function(){
+    document.getElementById("vermas3").style = 'display:none';
+    document.getElementById("btnvermenos3").style = 'display:none';
+    document.getElementById("btnvermas3").style = 'display:block';
+})
+document.getElementById("btnvermenos4").addEventListener("click",function(){
+    document.getElementById("vermas4").style = 'display:none';
+    document.getElementById("btnvermenos4").style = 'display:none';
+    document.getElementById("btnvermas4").style = 'display:block';
+})
+document.getElementById("btnvermenos5").addEventListener("click",function(){
+    document.getElementById("vermas5").style = 'display:none';
+    document.getElementById("btnvermenos5").style = 'display:none';
+    document.getElementById("btnvermas5").style = 'display:block';
+})
